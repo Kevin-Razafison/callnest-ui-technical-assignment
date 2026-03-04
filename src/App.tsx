82de2805451624261a1ee8bf9c07a23dbbe0a0f7
Kevin-Razafison <angelarknarem@gmail.com>
2026-03-04 14:21:40 +0300

    feat: create a basic Leads page

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,36 @@
-import { useState } from 'react';
-import { authService } from './api/authService';
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import Leads from './pages/Leads';
 
-function App() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
-
-  const handleLogin = async (e: React.FormEvent) => {
-    e.preventDefault();
-    try {
-      const data = await authService.login(email, password);
-      setMessage(`Connecté ! Bienvenue ${data.email}`);
-    } catch (err: any) {
-      setMessage("Erreur : " + (err.response?.data?.message || "Identifiants invalides"));
-    }
-  };
-
-  return (
-    <div className="flex justify-center items-center bg-slate-900 p-4 min-h-screen text-white">
-      <form onSubmit={handleLogin} className="bg-slate-800 shadow-2xl p-8 border border-slate-700 rounded-xl w-full max-w-md">
-        <h2 className="mb-6 font-bold text-blue-400 text-2xl text-center">Connexion CallNest</h2>
-        <input 
-          type="email" placeholder="Email" 
-          className="bg-slate-900 mb-4 p-3 border border-slate-700 focus:border-blue-500 rounded outline-none w-full"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input 
-          type="password" placeholder="Mot de passe" 
-          className="bg-slate-900 mb-6 p-3 border border-slate-700 focus:border-blue-500 rounded outline-none w-full"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit" className="bg-blue-600 hover:bg-blue-500 py-3 rounded w-full font-bold transition">
-          Se connecter
-        </button>
-        {message && <p className="mt-4 text-yellow-400 text-sm text-center">{message}</p>}
-      </form>
-    </div>
-  );
+const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+    const isAuthenticated = !!localStorage.getItem('token');
+    return isAuthenticated ? <>{children}</> : <Navigate to="/login" />
 }
 
-export default App;
+function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* Public Route */}
+        <Route path="/login" element ={<Login />} />
+
+        {/* Protected Routes */}
+        <Route path="/" element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        } />
+
+        <Route path="/leads" element={
+          <ProtectedRoute>
+            <Leads />
+          </ProtectedRoute>
+        } />
+
+        {/* Fallback */}
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </Router>
+  )
+}
---
+++ src/pages/Leads.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import api from "../api/axios";
+import { div, tr } from "framer-motion/client";
+
+function Leads() {
+    const [leads, setLeads] = useState([]);
+
+    useEffect(() => {
+        api.get('/leads')
+            .then(res => setLeads(res.data))
+            .catch(err => console.error("Failed to fetch leads", err));
+        
+    }, []);
+
+    return (
+        <div className="bg-slate-900 p-8 min-h-screen text-white">
+            <h1 className="mb-6 font-bold text-blue-400 text-3xl">Leads Management</h1>
+            <div className="bg-slate-800 border border-slate-700 rounded-xl overflow-hidden">
+                <table className="w-full text-left">
+                    <thead className="ng-slate-700/50">    
+                        <tr>
+                            <th className="p-4">Name</th>
+                            <th className="p-4">Email</th>
+                            <th className="p-4">Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {leads.length > 0? leads.map((lead: any) => (
+                            <tr key={lead.id} className="hover:bg-slate-700/30 border-slate-700 border-t">
+                                <td className="p-4">{lead.name}</td>
+                                <td className="p-4">{lead.email}</td>
+                                <td className="p-4 text-sm">
+                                    <span className="bg-blue-900/50 px-2 py-1 rounded text-blue-400">{lead.status}</span>
+                                </td>
+                            </tr>
+                        )) : (
+                            <tr><td colSpan={3} className="p-4 text-slate-500 tet-center">No leads found.</td></tr>
+                        )}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+}
+
+export default Leads;
